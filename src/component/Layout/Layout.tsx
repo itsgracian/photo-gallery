f7244934d52657c593c8f21b229d0f9dbfd89641
itsgracian <gracian2020@gmail.com>
2020-10-21 10:43:08 +0200

    add header

--- src/component/Layout/Layout.tsx
+++ src/component/Layout/Layout.tsx
@@ -4,6 +4,25 @@ import './style.css';
 const Layout: FC = (props) => {
   return (
     <div className='layoutScreen'>
+      <div className='header'>
+        <ul className="list">
+          <li>
+            <a href='https://github.com/itsgracian/' target="_blank">
+              <span className='ion-social-github'></span>
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/itsgracian' target="_blank">
+              <span className='ion-social-twitter-outline'></span>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/gratien-tuyishimire-255469113/' target="_blank">
+              <span className='ion-social-linkedin'></span>
+            </a>
+          </li>
+        </ul>
+      </div>
       {props.children}
       <div className='bottom'>
         <div className='logo'>
